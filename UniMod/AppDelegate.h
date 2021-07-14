5d22075abae49eea85180701f2b8b70951f3eeeb
杨莹 <yangying.iris@bytedance.com>
2021-07-14 11:14:34 +0800

    Initial Commit

---
+++ UniMod/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  UniMod
+//
+//  Created by yangying on 2021/7/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UniMod/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  UniMod
+//
+//  Created by yangying on 2021/7/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
